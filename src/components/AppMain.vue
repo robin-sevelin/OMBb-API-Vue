1a94f7f65dff7950c859683243479d1d794596f4
robin-sevelin <robinsevelin@hotmail.com>
2023-05-21 23:49:17 +0200

    :sparkles: fetch working

---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import axios from 'axios';
+
+  defineProps<{ msg: string }>();
+
+  const fetchApi = async () => {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?apikey=8b10bddd&/`
+    );
+
+    console.log(response);
+  };
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button @click="fetchApi">Skicka</button>
+</template>
+
+<style scoped></style>
